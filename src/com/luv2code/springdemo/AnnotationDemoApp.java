ef222331e020426532b4f8c3008876549741a563
Miguel.Baltazar <Miguel.Baltazar@AMMXDT15966.GEO.CORP.HCL.IN>
2016-11-28 11:47:07 -0600

    Spring Annotation "@component" and add tag content-arg- scan for get thoes annotation into the project

---
+++ src/com/luv2code/springdemo/AnnotationDemoApp.java
@@ -0,0 +1,22 @@
+package com.luv2code.springdemo;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class AnnotationDemoApp {
+
+	public static void main(String[] args) {
+		
+		//read Spring config File
+		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+		//get the bean from spring Container
+		
+		Coach theCoach =  context.getBean("thatSillyCoach" , Coach.class);
+		
+		//call a method on the bean
+		System.out.println(theCoach.getDailyWorkOut());
+		
+		//close the context
+		context.close();
+	}
+
+}
